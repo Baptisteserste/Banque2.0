824df36a005f1fd9f79be7d3144cdd1d7ce2b34e
Baptisteserste <baptiste.serste@gmail.com>
2024-10-12 12:27:40 +0200

    Banquev1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿class Personne
+{
+    public string Nom { get; private set; }
+    public string Prenom { get; private set; }
+    public int Age { get; private set; }
+    public int Taille { get; private set; }
+
+    public Personne(string nom, string prenom, int age, int taille)
+    {
+        Nom = nom;
+        Prenom = prenom;
+        Age = age;
+        Taille = taille;
+    }
+}
